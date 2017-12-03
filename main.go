c2cf056790bf5c8ab3271b4918a414c9ef070f57
b4b4r07 <b4b4r07@gmail.com>
2017-12-03 21:59:50 +0900

    Fix small bugs and a bit of refactoring

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ const (
 const helpText string = `Usage: curl
 
 Extended options:
-  --list-urls    List service URLs
-  --edit-config  Edit config file
+  --list, --list-urls    List service URLs
+  --edit, --edit-config  Edit config file
 `
 
 var (
@@ -45,46 +45,49 @@ func main() {
 }
 
 func run(args []string) int {
-	var url string
-	if len(args) > 0 {
-		switch args[0] {
-		case "-h", "--help":
-			fmt.Fprint(os.Stderr, helpText)
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "Error: too few arguments\n")
+		return 1
+	}
+	switch args[0] {
+	case "-h", "--help":
+		fmt.Fprint(os.Stderr, helpText)
+		return 1
+	case "--list-urls", "--list":
+		fmt.Println(strings.Join(cfg.GetURLs(), "\n"))
+		return 0
+	case "--edit-config", "--edit":
+		if err := cfg.Edit(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err.Error())
 			return 1
-		case "--list-urls":
-			fmt.Println(strings.Join(cfg.GetURLs(), "\n"))
-			return 0
-		case "--edit-config":
-			err := cfg.Edit()
-			if err == nil {
-				return 0
-			}
-			fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
-			return 1
-		default:
-			// url = args[0]
 		}
+		return 0
+	default:
+		// Ignore other arguments
+	}
 
-		url = args[len(args)-1]
-
-		env, err := cfg.GetEnv(url)
-		if err != nil {
+	// The last argument is regarded as an URL
+	url := args[len(args)-1]
+	env, err := cfg.GetEnv(url)
+	if err != nil {
+		similarURLs := cfg.SimilarURLs(url)
+		if len(similarURLs) > 0 {
 			fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
-			fmt.Fprintf(os.Stderr, "       similar urls found %q\n", cfg.SimilarURLs(url))
+			fmt.Fprintf(os.Stderr, "       similar urls found %q\n", similarURLs)
 			return 1
 		}
-		credentials = os.Getenv(GoogleApplicationCredentials)
-		clientID = os.Getenv(IAPClientID)
-		binary = os.Getenv(IAPCurlBinary)
-		if credentials == "" {
-			credentials, _ = homedir.Expand(env.Credentials)
-		}
-		if clientID == "" {
-			clientID = env.ClientID
-		}
-		if binary == "" {
-			binary = env.Binary
-		}
+	}
+	credentials = os.Getenv(GoogleApplicationCredentials)
+	clientID = os.Getenv(IAPClientID)
+	binary = os.Getenv(IAPCurlBinary)
+	if credentials == "" {
+		credentials, _ = homedir.Expand(env.Credentials)
+	}
+	if clientID == "" {
+		clientID = env.ClientID
+	}
+	if binary == "" {
+		binary = env.Binary
 	}
 
 	if credentials == "" {
@@ -107,10 +110,8 @@ func run(args []string) int {
 
 	authHeader := fmt.Sprintf("'Authorization: Bearer %s'", token)
 	curlArgs := append(
-		// For IAP header
-		[]string{"-H", authHeader},
-		// Original args
-		args...,
+		[]string{"-H", authHeader}, // For IAP header
+		args..., // Original args
 	)
 
 	if err := doCurl(binary, curlArgs); err != nil {
